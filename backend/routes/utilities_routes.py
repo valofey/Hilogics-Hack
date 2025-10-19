2d2ace8eea0be3f95cbc7fc3a7bbd9a631011bce
Ilya Dashevsky <idashevskii@gmail.com>
2025-10-19 09:07:10 +0300

    added historical similarities endpoint

--- backend/routes/utilities_routes.py
+++ backend/routes/utilities_routes.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, HTTPException, Header
+from pathlib import Path
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 import httpx
 from config import settings
+import json
 
 DADATA_URL = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/findById/party"
 
@@ -11,6 +13,26 @@ class PartyQuery(BaseModel):
 
 
 router = APIRouter()
+
+
+@router.get("/historical-similarities")
+async def get_historical_similarities():
+    """
+    Read and return JSON data from ./data/historical_similarities.json
+    """
+    file_path = Path("./data/historical_similarities.json")
+
+    if not file_path.exists():
+        raise HTTPException(status_code=404, detail="File not found")
+
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+        return data
+    except json.JSONDecodeError:
+        raise HTTPException(status_code=500, detail="Invalid JSON format")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error reading file: {str(e)}")
 
 
 @router.post("/company-info/")
